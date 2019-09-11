e9397b72f78205834f85084e7f5e5303375461db
PabloFS1315 <pablofernandezsilva15@gmail.com>
2019-09-11 14:17:45 +0200

    Suma V.1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,15 @@
 /* Here, you should entry the required libraries */
+#include <stdio.h>
+
 
 void main(){
-	/* Here, you should write the source code of your program */
+	int a,b,R;
+  printf("Enter a value");
+  scanf("%d", &a);
+  printf("Enter other value");
+  scanf("%d", &b);
+  
+	R = a + b;
+	
+	printf("The value is %d",R);
 }
