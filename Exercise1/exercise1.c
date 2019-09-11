fa294657c0c4f514bcde6df71bd91d563574415a
PabloFS1315 <pablofernandezsilva15@gmail.com>
2019-09-11 14:02:29 +0200

    Version 1

--- Exercise1/exercise1.c
+++ Exercise1/exercise1.c
@@ -2,8 +2,9 @@
 /* Implement a program that it shows the message "Hello World" on screen. */
 /*************************************************************************/
 
-/* Here, you must include the required libraries */
 
+
+#include <stdio.h>
 void main(){
-	/* Here, you must write the source code to show the missatge "Hello world" on screen*/
+printf("Hello World Version 1");
 }
